0b902c98a741428002157972ea20930aae9a62e1
yhddd123 <gdfyhmyhm@gmail.com>
2024-11-23 16:38:41 +0800

    add gridea 24-11-23

---
+++ Gridea/Comments.cpp
@@ -0,0 +1,7 @@
+<script src="https://utteranc.es/client.js"
+        repo="yhddd123/yhddd123.github.io"
+        issue-term="pathname"
+        theme="github-light"
+        crossorigin="anonymous"
+        async>
+</script>
